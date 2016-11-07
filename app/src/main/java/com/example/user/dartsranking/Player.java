8bad4583a77f8aa032d6f78cf6f2465b97fe41a2
katemanson <katemanson107@hotmail.com>
2016-11-07 15:31:01 +0000

    initial commit

---
+++ app/src/main/java/com/example/user/dartsranking/Player.java
@@ -0,0 +1,19 @@
+package com.example.user.dartsranking;
+
+/**
+ * Created by user on 07/11/2016.
+ */
+
+public class Player {
+
+    private String name;
+    private String nickname;
+    private int currentRanking;
+
+    public Player(String name, String nickname, int currentRanking) {
+        this.name = name;
+        this.nickname = nickname;
+        this.currentRanking = currentRanking;
+    }
+
+}
